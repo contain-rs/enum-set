7ebaf6f5f5a10712231f01a51f815f1ed8635b12
FlashCat <FlashCat@users.noreply.github.com>
2015-03-26 18:25:23 +0000

    rebuild pages at ee27d37

--- implementors/core/cmp/trait.PartialOrd.js
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['enum-set'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;E: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='enum-set/struct.EnumSet.html' title='enum-set::EnumSet'>EnumSet</a>&lt;E&gt;",];
+implementors['enum_set'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;E: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='enum_set/struct.EnumSet.html' title='enum_set::EnumSet'>EnumSet</a>&lt;E&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
